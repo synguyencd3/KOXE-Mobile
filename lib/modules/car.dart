63065c3cf3b656c2aeb338017476e5d58c473dc2
namkhanh1234 <20120115@student.hcmus.edu.vn>
2024-02-28 23:20:41 +0700

    homepage 1

---
+++ lib/modules/car.dart
@@ -0,0 +1,14 @@
+class Car {
+  final int id;
+  final String name;
+  final String description;
+  final String image;
+  final double price;
+  Car({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.image,
+    required this.price,
+  });
+}
---
+++ lib/modules/page.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class PageModule {
+  Widget page;
+  String label;
+  PageModule({required this.page, required this.label});
+}
