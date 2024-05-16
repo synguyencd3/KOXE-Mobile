396523591dffdb77d7a2f5712640f8505a1bc424
namkhanh1234 <20120115@student.hcmus.edu.vn>
2024-05-16 16:52:32 +0700

    Khanh add connection API

--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -2,14 +2,14 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:mobile/config.dart';
+import 'package:mobile/model/transaction_model.dart';
 import 'package:mobile/services/api_service.dart';
 import 'package:mobile/services/shared_service.dart';
-import 'package:mobile/model/connection_model.dart';
 
 class TransactionService {
   static var client = http.Client();
 
-  static Future<List<ConnectionModel>> getTransactions() async {
+  static Future<List<TransactionModel>> getTransactions() async {
     await APIService.refreshToken();
     var loginDetails = await SharedService.loginDetails();
     Map<String, String> requestHeaders = {
@@ -20,8 +20,8 @@ class TransactionService {
     var response = await http.get(url, headers: requestHeaders);
     if (response.statusCode == 200) {
       var data = jsonDecode(response.body);
-      print(data);
-      return connectionsFromJson(data['connections']);
+      //print(data);
+      return transactionsFromJson(data['transaction']);
     }
     print('error');
     return [];
